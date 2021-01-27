5bf7274658c90bce6f5b278ed4ce93fb546cbfa2
Lanboku <m.hirayama0129@gmail.com>
2021-01-28 08:04:26 +0900

    【Update】cd の後に自動で ls を行う設定を追加

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,6 +9,13 @@ zle -N peco-history-selection
 bindkey '^R' peco-history-selection
 
 #################################  CDR  #################################
+# cd after ls
+chpwd() {
+    if [[ $(pwd) != $HOME ]]; then;
+        lsd -la
+    fi
+}
+
 if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
     autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
     add-zsh-hook chpwd chpwd_recent_dirs
